6735c2459b66d2290a7cc2c1bb23173e0941625c
Franco Caraffa <francocaraffa2005@gmail.com>
2023-11-10 10:24:49 -0300

    different routes (http module)

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const http = require('node:http')
+const fs = require('node:fs')
 const PORT = 1234
 
 const server = http.createServer((req, res)=>{
   if(req.url === '/'){
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    res.end('<h1>Holá mundo</h1>')
+    res.end('<h1>Hola mundo</h1>')
+  }else if(req.url === '/contacto'){
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("<h1>Contacto</h1>");
+  }else if(req.url === "/imagen-corazon.png"){
+    fs.readFile('./corazon-amarillo.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>Internal server errror</h1>')
+      } else {
+        res.setHeader("Content-Type", "image/png");
+        res.end(data)       
+      }
+    })
+  } else{
+    res.statusCode = 404
+    res.end('<h1>404</h1>')
   }
 })
 
