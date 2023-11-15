5ed3e8b82041a9f617f9cdc00a8dfda0803ed9f6
Franco Caraffa <francocaraffa2005@gmail.com>
2023-11-15 13:01:14 -0300

    express post

--- express.js
+++ express.js
@@ -7,6 +7,26 @@ app.get('/', (req, res)=>{
     res.send('<h1>Mi pagina</h1>')
 })
 
+app.get('/json', (req, res)=>{
+    // content type y stringify automatico
+    res.json({message: 'Hola'})
+})
+
+app.post('/pokemon', (req, res)=>{
+    let body = ''
+
+    // escuchar el evento data
+    req.on('data', chunk => {
+        body += chunk.toString()
+    })
+
+    req.on('end', () => {
+        const data = JSON.parse(body)
+        console.log(data)
+        res.status(201).json(data)
+    })
+})
+
 app.listen(PORT, ()=>{
     console.log('Server listening')
 })
